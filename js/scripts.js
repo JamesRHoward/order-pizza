823806341755a6e9c5f906bbec72233e2af65eab
James Howard <james@>
2016-06-17 11:28:24 -0700

    fixed business logic bug. li of ordered pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 // Business/front end logic.
-function Pizza(size, topping) {
+function Pizza(size, topping, topping2, topping3) {
   this.psize = size;
   this.topping = topping;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
   this.price = 20;
 }
 Pizza.prototype.toppingPrice = function() {
-  if (this.topping === "mushrooms" || this.topping === "onions" || this.topping === "pineapple" || this.topping === "olives") {
-    return this.price;
-  } else if (this.topping === "bacon" || this.topping === "cbacon" || this.topping === "pepperoni" || this.topping === "bbq" || this.topping === "sausage") {
-    return this.price +=1;
+  if (this.topping === "bacon" || this.topping === "canadian-bacon" || this.topping === "pepperoni" || this.topping === "bbq" || this.topping === "sausage") {
+    this.price +=1;
+  } if (this.topping2 === "bacon" || this.topping2 === "canadian-bacon" || this.topping2 === "pepperoni" || this.topping2 === "bbq" || this.topping2 === "sausage") {
+    this.price +=1;
+  } if (this.topping3 === "bacon" || this.topping3 === "canadian-bacon" || this.topping3 === "pepperoni" || this.topping3 === "bbq3" || this.topping3 === "sausage") {
+    this.price +=1;
   }
   return this.price
 }
 
-Pizza.prototype.pizzaPrice = function() {
+Pizza.prototype.sizePrice = function() {
   if (this.psize === "12") {
     return this.price -= 10;
   } else if (this.psize === "16") {
@@ -24,19 +28,28 @@ Pizza.prototype.pizzaPrice = function() {
   return this.price;
 }
 
+Pizza.prototype.pizzaPrice = function() {
+  this.psize + this.topping + this.topping2 + this.topping3;
+}
+
 // User Interface/back end logic.
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
 
     var size = $("select#size").val();
-    var toppings = $("select.toppings").val();
-    var newPizza = new Pizza(size, toppings);
+    var topping1 = $("select.toppings1").val();
+    var topping2 = $("select.toppings2").val();
+    var topping3 = $("select.toppings3").val();
+    var newPizza = new Pizza(size, topping1, topping2, topping3);
     newPizza.pizzaPrice();
     newPizza.toppingPrice();
+    newPizza.sizePrice();
 
     $(".size").text(newPizza.psize);
-    $(".toppings").text(newPizza.topping);
+    $(".toppings1").text(newPizza.topping);
+    $(".toppings2").text(newPizza.topping2);
+    $(".toppings3").text(newPizza.topping3);
     $(".price").text(newPizza.price);
     $("#results").show();
   })

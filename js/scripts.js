73d725e420cc27921d898ad364935937ff690fb9
James Howard <james@>
2016-06-17 09:58:23 -0700

    added front end logic. new prototype for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function Pizza(size, topping) {
 }
 
 Pizza.prototype.pizzaPrice = function(){
-  if (this.psize === "12" && this.topping === "toppings") {
+  if (this.psize === "12" && this.topping === "bacon") {
     return this.price -= 10;
   } else if (this.psize === "16" && this.topping === "toppings") {
     return this.price -= 6;
@@ -15,3 +15,20 @@ Pizza.prototype.pizzaPrice = function(){
   }
   return this.price;
 }
+
+// User Interface/back end logic.
+$(document).ready(function() {
+  $("form#pizza").submit(function(event) {
+    event.preventDefault();
+
+    var size = $("select#size").val();
+    var toppings = $("select#toppings").val();
+    var newPizza = new Pizza(size, toppings);
+    newPizza.pizzaPrice();
+
+    $(".size").text(newPizza.size);
+    $(".toppings").text(newPizza.topping);
+    $(".price").text(newPizza.price);
+    $("#results").show();
+  })
+})
